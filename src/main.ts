f595512e929d3abb322a15bd3666fe2e0411b853
hjyoon <hjyoon@kakao.com>
2024-11-11 01:47:05 +0900

    feat: Setup ConfigModule with @nestjs/config

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const port = process.env.PORT ?? 5000;
-  console.log(port);
-  await app.listen(port);
+  await app.listen(process.env.PORT);
 
   if (process.send) {
     process.send('ready');
