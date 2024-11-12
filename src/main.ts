212c458ba41fb167ace39f23c25319d1df390504
hjyoon <hjyoon@kakao.com>
2024-11-13 06:15:18 +0900

    refactor: Use ConfigService for dynamic port configuration in main bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.setGlobalPrefix('/api');
-
   app.enableShutdownHooks(['SIGINT', 'SIGTERM']);
 
-  await app.listen(process.env.PORT);
+  const port = configService.get<number>('PORT');
+  await app.listen(port);
 
   if (process.send) {
     process.send('ready');
+    console.log('Worker process is ready.');
   }
+
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
